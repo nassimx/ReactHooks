b2f6ddb72ff8290f5553b720c1114a207360d411
nassimx <mr.nassimfr@gmail.com>
2021-07-04 14:41:45 +0100

    2

---
+++ src/Components/Data.js
@@ -0,0 +1,53 @@
+const data = {
+    movieData: [
+        {
+            title: "The Shawshank Redemption",
+            desc: "desc1",
+            id: 1,
+            imgSrc: "https://m.media-amazon.com/images/M/MV5BMTUxMzQyNjA5MF5BMl5BanBnXkFtZTYwOTU2NTY3._V1_QL100_UX258.jpg"
+        },
+        {
+            title: "abc",
+            desc: "desc1",
+            id: 2,
+            imgSrc: "https://m.media-amazon.com/images/M/MV5BMTUxMzQyNjA5MF5BMl5BanBnXkFtZTYwOTU2NTY3._V1_QL100_UX258.jpg"
+        },
+        {
+            title: "tt",
+            desc: "dd",
+            id: 3,
+            imgSrc: "https://m.media-amazon.com/images/M/MV5BMTUxMzQyNjA5MF5BMl5BanBnXkFtZTYwOTU2NTY3._V1_QL100_UX258.jpg"
+        },
+        {
+            title: "vv",
+            desc: "abc",
+            id: 4,
+            imgSrc: "https://m.media-amazon.com/images/M/MV5BMTUxMzQyNjA5MF5BMl5BanBnXkFtZTYwOTU2NTY3._V1_QL100_UX258.jpg"
+        },
+        {
+            title: "cc",
+            desc: "desc1",
+            id: 5,
+            imgSrc: "https://m.media-amazon.com/images/M/MV5BMTUxMzQyNjA5MF5BMl5BanBnXkFtZTYwOTU2NTY3._V1_QL100_UX258.jpg"
+        },
+        {
+            title: "The Shawshank Redemption",
+            desc: "desc1",
+            id: 6,
+            imgSrc: "https://m.media-amazon.com/images/M/MV5BMTUxMzQyNjA5MF5BMl5BanBnXkFtZTYwOTU2NTY3._V1_QL100_UX258.jpg"
+        },
+        {
+            title: "The Shawshank Redemption",
+            desc: "desc1",
+            id: 7,
+            imgSrc: "https://m.media-amazon.com/images/M/MV5BMTUxMzQyNjA5MF5BMl5BanBnXkFtZTYwOTU2NTY3._V1_QL100_UX258.jpg"
+        },
+        {
+            title: "The Shawshank Redemption",
+            desc: "desc1",
+            id: 8,
+            imgSrc: "https://m.media-amazon.com/images/M/MV5BMTUxMzQyNjA5MF5BMl5BanBnXkFtZTYwOTU2NTY3._V1_QL100_UX258.jpg"
+        }
+    ]
+}
+export default data;
